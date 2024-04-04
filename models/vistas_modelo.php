396142747ecd157a6f67e4d65c10d4b3b73aa6fc
Oscar Raygoza <oscar.raygoza@gmail.com>
2024-04-04 16:53:59 -0600

    Arreglando rutas

--- models/vistas_modelo.php
+++ models/vistas_modelo.php
@@ -2,7 +2,11 @@
     class vistas_modelo{
         /*-----------Obtener Vistas -------- */
         protected static function get_view_model($vistas){
-            $white_list=["home","client-list","client-new"];
+            $white_list=["home","client-list","client-new","client-list","client-search",
+                        "client-update","company","product-list","product-new","product-search",
+                    "product-update","reports","reservation-approved","reservation-list",
+                    "reservation-new","reservation-new","reservation-search","reservation-status",
+                    "reservation-update","user-list","user-new","user-search","user-update"];
             if(in_array($vistas, $white_list)){
                 if(is_file("./views/contents/".$vistas."-view.php")){
                     $content="./views/contents/".$vistas."-view.php";
