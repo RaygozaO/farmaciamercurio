c1d2bfb256d430292ae4e6ee80e05e4698180c4a
Oscar Raygoza <oscar.raygoza@gmail.com>
2024-04-04 17:16:48 -0700

    arreglando rama

--- config/APP.php
+++ config/APP.php
@@ -1,9 +1,5 @@
 <?php 
-<<<<<<< HEAD
-    const SERVERURL="";
-=======
     const SERVERURL="http://www.farmaciamercurio.com.mx/";
->>>>>>> b84448b (desde ubuntu)
     const COMPANY="Farmacia Mercurio";
     const MONEDA="$";
     date_default_timezone_set('America/Mexico_City');
