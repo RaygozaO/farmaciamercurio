957250f37d5a08b007eac3c2fc733124d75a1f8d
Oscar Raygoza <oscar.raygoza@gmail.com>
2024-04-04 19:58:34 -0600

    rutas otra vez

--- config/APP.php
+++ config/APP.php
@@ -1,5 +1,5 @@
 <?php 
-    const SERVERURL="www.farmaciamercurio.com.mx";
+    const SERVERURL="";
     const COMPANY="Farmacia Mercurio";
     const MONEDA="$";
     date_default_timezone_set('America/Mexico_City');
